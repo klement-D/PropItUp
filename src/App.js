889928a0e14cb43ce851fc85ae291b8658a6c95f
Klement Doda <klement.doda@gmail.com>
2023-04-24 14:52:35 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,43 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+
+function PersonCard(props) {
+  return (
+    <div>
+      <h2>{props.firstName}, {props.lastName}</h2>
+      <p>Age: {props.age}</p>
+      <p>Hair Color: {props.hairColor}</p>
+    </div>
+  );
+}
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <PersonCard 
+        firstName="Doe" 
+        lastName="Jane" 
+        age="45" 
+        hairColor="Black" 
+      />
+      <PersonCard 
+        firstName="Smith" 
+        lastName="John" 
+        age="88" 
+        hairColor="Brown" 
+      />
+      <PersonCard 
+        firstName="Fillmore" 
+        lastName="Millard" 
+        age="50" 
+        hairColor="Brown" 
+      />
+      <PersonCard 
+        firstName="Smith" 
+        lastName="Maria" 
+        age="62" 
+        hairColor="Brown" 
+      />
     </div>
   );
 }
